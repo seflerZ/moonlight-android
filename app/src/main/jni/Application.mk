1994030881976d273b56142ef93262be7213b95d
sefler <sefler@126.com>
2025-06-09 09:23:47 +0800

    change Application.mk in moonlight

--- app/src/main/jni/Application.mk
+++ app/src/main/jni/Application.mk
@@ -1,4 +1,7 @@
 # Application.mk for Moonlight
 
-# Our minimum version is Android 4.1
-APP_PLATFORM := android-16
+APP_PLATFORM := android-21
+APP_OPTIM := release
+APP_DEBUG := false
+APP_ABI := arm64-v8a
+APP_STL := c++_shared
