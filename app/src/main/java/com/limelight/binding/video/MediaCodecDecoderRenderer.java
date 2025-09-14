13831a11f42b2961b5b1e82bf04bb9106b99a48c
sefler <sefler@126.com>
2025-09-14 15:14:15 +0800

    increase buffer queue

--- app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
+++ app/src/main/java/com/limelight/binding/video/MediaCodecDecoderRenderer.java
@@ -125,7 +125,7 @@ public class MediaCodecDecoderRenderer extends VideoDecoderRenderer implements C
     private PreferenceConfiguration prefs;
     private Surface surface;
     private LinkedBlockingQueue<Integer> outputBufferQueue = new LinkedBlockingQueue<>();
-    private static final int OUTPUT_BUFFER_QUEUE_LIMIT = 8;
+    private static final int OUTPUT_BUFFER_QUEUE_LIMIT = 10;
     private long lastRenderedFrameTimeNanos;
     private HandlerThread choreographerHandlerThread;
     private Handler choreographerHandler;
