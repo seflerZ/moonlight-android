b60751fb6d2ea0e4845adafa7773cfef8eca5a12
sefler <sefler@126.com>
2025-11-02 18:11:02 +0800

    try to enable support for 16KB memory

--- app/src/main/jni/moonlight-core/openssl/Android.mk
+++ app/src/main/jni/moonlight-core/openssl/Android.mk
@@ -1,3 +1,15 @@
 LOCAL_PATH:= $(call my-dir)
  
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libssl
+LOCAL_SRC_FILES:= $(TARGET_ARCH_ABI)/libssl.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384,--exclude-libs,ALL
+include $(PREBUILT_STATIC_LIBRARY)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libcrypto
+LOCAL_SRC_FILES:= $(TARGET_ARCH_ABI)/libcrypto.a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_LDFLAGS += -Wl,-z,max-page-size=16384,--exclude-libs,ALL
+include $(PREBUILT_STATIC_LIBRARY)
